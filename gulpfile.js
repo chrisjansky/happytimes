a3cf8ad82085fbed5388db3bb3f29e73eaec7db9
chrisjansky <c.jansky@de.edenspiekermann.com>
2014-11-26 22:05:04 +0100

    Updated gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,15 @@
 var
   fs = require("fs"),
   argv = require("yargs").argv,
-  glob = require("glob"),
-
   gulp = require("gulp"),
-  bsync = require("browser-sync"),
-  hygienist = require("hygienist-middleware"),
-
-  clean = require("gulp-clean"),
-  size = require("gulp-size"),
-  rename = require("gulp-rename"),
-  watch = require("gulp-watch"),
-  plumber = require("gulp-plumber"),
-
-  sass = require("gulp-sass"),
-  jade = require("gulp-jade"),
-
-  autoprefix = require("gulp-autoprefixer"),
-  usemin = require("gulp-usemin"),
-  imagemin = require("gulp-imagemin"),
-  minify = require("gulp-minify-css"),
-  uglify = require("gulp-uglify"),
-  uncss = require("gulp-uncss"),
-
-  svgpng = require("gulp-svg2png"),
-  svgsprite = require("gulp-svg-sprites");
-  // gulpkss = require("gulp-kss"),
+  plugins = require('gulp-load-plugins')({
+    pattern: ["glob", "gulp-*", "browser-*", "hygienist-*"]
+  });
 
 var paths = {
   development: ".",
   production: "public/",
+  styleguide: "public/styleguide/",
 
   pages: "*.html",
   jade: "assets/jade/*.jade",
@@ -42,132 +22,94 @@ var paths = {
   glob_css: "assets/css/*.css",
 
   js: "assets/js/*.js",
+  glob_js: "assets/js/**/*.js",
 
   images: "assets/images",
-  fallbacks: "assets/images/fallbacks",
   glob_images: "assets/images/**/*",
   ignore_images: "!assets/images/ignore{,/**}",
-  
-  svg: "assets/svg",
-  glob_svg: "assets/svg/source/*",
 
-  glob_data: "assets/data/*.json"
-  // kss: "assets/kss/*.html",
-  // kss_css: "assets/scss/kss.scss",
+  glob_svg: "assets/svg/*",
+
+  fallbacks: "assets/images/fallbacks",
+
+  glob_data: "assets/data/*.json",
+
+  kss: "assets/kss",
+  kss_css: "assets/css/styleguide.css"
 };
 
-// Declare files to move to public/ during "build" task.
-var productionFiles = [
-  "assets/fonts/*",
-  "assets/favicon.ico"
-];
+/*
+------------------------------ Basic. -----------------------------
+*/
 
-// Super-dumb JSON concatenation. Winning!
-var
-  jsonFiles,
-  jsonGroup;
-
-gulp.task("fetch-data", function() {
-  jsonFiles = glob.sync(paths.glob_data),
-  jsonGroup = fs.readFileSync(jsonFiles[0], "utf8");
-
-  // Add other files if more than one.
-  if (jsonFiles.length > 1) {
-    for (i = 1; i < jsonFiles.length; i++) {
-      jsonGroup += ",\n" + fs.readFileSync(jsonFiles[i], "utf8");
-    }
-  }
-  // Make an array out of it.
-  jsonGroup = "{" + jsonGroup + "}";
+gulp.task("server", function() {
+  plugins.browserSync({
+    server: {
+      baseDir: paths.development,
+      middleware: plugins.hygienistMiddleware(paths.development)
+    },
+    xip: true,
+    notify: false
+  });
 });
 
-// Compile SCSS to CSS.
 gulp.task("styles", function () {
   return gulp.src(paths.scss)
-    .pipe(plumber())
-    .pipe(sass({
+    .pipe(plugins.plumber())
+    .pipe(plugins.sass({
       errLogToConsole: true,
       includePaths: require("node-neat").with("bower_components/")
     }))
     .pipe(gulp.dest(paths.css))
-    .pipe(bsync.reload({stream: true}));
+    .pipe(plugins.browserSync.reload({
+      stream: true
+    }));
 });
 
-gulp.task("pages", ["templates"], function() {
-  bsync.reload();
-});
-
-gulp.task("templates", ["fetch-data"], function() {
+gulp.task("templates", function() {
   return gulp.src([paths.glob_jade, paths.ignore_jade])
-    .pipe(plumber())
-    .pipe(jade({
-      pretty: true,
-      locals: JSON.parse(jsonGroup)
+    .pipe(plugins.plumber())
+    .pipe(plugins.jade({
+      pretty: true
     }))
     .pipe(gulp.dest(paths.development));
 });
 
-// gulp.task("kss", function() {
-//   gulp.src(paths.glob_scss)
-//     .pipe(gulpkss({
-//       overview: "assets/kss/styleguide.md",
-//       templateDirectory: "assets/kss/"
-//     }))
-//     .pipe(gulp.dest("public/styleguide"));
-//   gulp.src(paths.kss_css)
-//     .pipe(sass({
-//       errLogToConsole: true,
-//       includePaths: require("node-neat").with("bower_components/")
-//     }))
-//     .pipe(minify())
-//     .pipe(rename("kss-min.css"))
-//     .pipe(gulp.dest(paths.production + paths.css));
-// });
-
-// Delete the assets/fallbacks/ folder.
-gulp.task("svg-wipe", function() {
-  return gulp.src(paths.fallbacks, {read: false})
-    .pipe(clean());
+gulp.task("pages", ["templates"], function() {
+  plugins.browserSync.reload();
 });
 
-// Render PNG fallbacks for SVG.
-gulp.task("svg-fallback", ["svg-wipe"], function() {
-  return gulp.src(paths.glob_svg)
-    .pipe(svgpng())
-    .pipe(gulp.dest(paths.fallbacks));
+gulp.task("scan", function () {
+  // Using gulp.start soon to be deprecated.
+  plugins.watch(paths.glob_scss, function(files, cb) {
+    gulp.start("styles", cb);
+  });
+  plugins.watch([paths.glob_jade, paths.glob_data, paths.glob_js], function(files, cb) {
+    gulp.start("pages", cb);
+  });
 });
 
-// Combine all SVG assets into one.
-gulp.task("svg-combine", function() {
-  return gulp.src(paths.glob_svg)
-    .pipe(svgsprite({
-      mode: "symbols",
-      // svgId: "svg-%f",
-      // preview: false,
-      svg: {
-        symbols: "complete.svg"
-      },
-      preview: {
-        symbols: "index.html"
-      }
-    }))
-    .pipe(size({
-      showFiles: true
-    }))
-    .pipe(gulp.dest(paths.svg));
-})
+/*
+---------------------------- Build. ----------------------------
+*/
+
+var productionFiles = [];
+
+// Delete the previous build.
+gulp.task("build-wipe", function() {
+  if (argv.full) {
+    return gulp.src(paths.production, {read: false})
+      .pipe(plugins.clean());
+  } else return;
+});
 
 // Minify CSS and JS.
-gulp.task("produce", ["wipe"], function() {
+gulp.task("build-compile", ["build-wipe"], function() {
   return gulp.src(paths.pages)
-    .pipe(plumber())
-    .pipe(usemin({
-      js: [uglify()],
+    .pipe(plugins.usemin({
+      js: [plugins.uglify()],
       css: [
-        autoprefix({
-          cascade: false
-        }),
-        minify({
+        plugins.minifyCss({
           keepSpecialComments: 0
         })]
     }))
@@ -175,24 +117,15 @@ gulp.task("produce", ["wipe"], function() {
 });
 
 // Move other assets to production folder.
-gulp.task("move", ["wipe"], function() {
+gulp.task("build-move", ["build-wipe"], function() {
   gulp.src(productionFiles, {base: paths.development})
     .pipe(gulp.dest(paths.production));
 });
 
-// Delete the previous build if provided with --full argument.
-gulp.task("wipe", function() {
-  if (argv.full) {
-    return gulp.src(paths.production, {read: false})
-      .pipe(clean());
-  } else return;
-});
-
 // Minify images if provided with --full argument.
-gulp.task("images", ["wipe"], function() {
+gulp.task("build-images", ["build-wipe"], function() {
   if (argv.full) {
     return gulp.src([paths.glob_images, paths.ignore_images])
-      .pipe(plumber())
       .pipe(imagemin({
         progressive: true,
         svgoPlugins: [{removeViewBox: false}]
@@ -201,61 +134,86 @@ gulp.task("images", ["wipe"], function() {
   };
 });
 
-// Wait for "produce" and strip unused CSS afterwards if --uncss provided.
-gulp.task("strip-css", ["produce"], function() {
+// Strip unused CSS afterwards if --uncss provided.
+gulp.task("build-strip", ["build-compile"], function() {
   if (argv.uncss) {
     return gulp.src(paths.production + paths.glob_css)
-      .pipe(plumber())
-      .pipe(uncss({
-        html: glob.sync(paths.pages),
-        // Ignore pseudo selectors.
+      .pipe(plugins.uncss({
+        html: plugins.glob.sync(paths.pages),
         ignore: [/::?-[\w\d]+/]
       }))
-      .pipe(minify())
-      .pipe(size({
+      .pipe(plugins.minifyCss({
+        keepSpecialComments: 0
+      }))
+      .pipe(plugins.size({
         showFiles: true
       }))
       .pipe(gulp.dest(paths.production + paths.css));
   }
 });
 
-// Wait for "compile" before starting up server.
-gulp.task("server", ["compile"], function() {
-  bsync({
-    server: {
-      baseDir: paths.development,
-      middleware: hygienist(paths.development)
-    },
-    xip: true,
-    notify: false
-  });
+/*
+----------------------------- SVG. ------------------------------
+*/
+
+// Delete the PNG fallbacks/ folder.
+gulp.task("svg-wipe", function() {
+  return gulp.src(paths.fallbacks, {read: false})
+    .pipe(plugins.clean());
 });
 
-gulp.task("scan", function () {
-  // Traditional watch not working.
-  // gulp.watch(paths.glob_scss, ["styles"]);
-  // gulp.watch(paths.glob_jade, ["templates"]);
-  // gulp.watch(paths.data, ["templates"]);
-
-  // Using gulp.start, soon to be deprecated
-  watch(paths.glob_scss, function() {
-    gulp.start("styles");
-  });
-  watch(paths.glob_jade, function() {
-    gulp.start("pages");
-  });
-  watch(paths.glob_data, function() {
-    gulp.start("pages");
-  });
-  watch(paths.js, function() {
-    bsync.reload();
-  });
-  // watch(paths.kss, ["kss"]);
+// Render PNG fallbacks for SVG.
+gulp.task("svg", ["svg-wipe"], function() {
+  return gulp.src(paths.glob_svg)
+    .pipe(plugins.svg2png())
+    .pipe(gulp.dest(paths.fallbacks));
 });
 
+/*
+-------------------------- Styleguide. --------------------------
+*/
+
+var styleguideFiles = [
+  "assets/kss/assets/prism.css"
+];
+
+gulp.task("styleguide-wipe", ["build-wipe"], function() {
+  return gulp.src(paths.styleguide, {read: false})
+    .pipe(plugins.clean());
+});
+
+gulp.task("styleguide-move", ["styleguide-wipe"], function() {
+  return gulp.src(styleguideFiles, {base: paths.kss})
+    .pipe(gulp.dest(paths.styleguide));
+});
+
+gulp.task("styleguide-styles", ["styleguide-move"], function() {
+  return gulp.src(paths.kss_css)
+    .pipe(plugins.minifyCss({
+      keepSpecialComments: 0
+    }))
+    .pipe(plugins.rename(function (path) {
+        path.basename += "-min";
+    }))
+    .pipe(gulp.dest(paths.production + paths.css));
+});
+
+gulp.task("styleguide-compile", ["styleguide-styles"], function() {
+  return gulp.src(paths.glob_scss)
+    .pipe(plugins.kss({
+      overview: paths.kss + "/styleguide.md",
+      templateDirectory: paths.kss
+    }))
+    .pipe(gulp.dest(paths.styleguide));
+});
+
+/*
+-------------------------- Task groups. ---------------------------
+*/
 gulp.task("default", ["compile", "server", "scan"]);
-gulp.task("svg", ["svg-fallback", "svg-combine"]);
 gulp.task("compile", ["styles", "pages"]);
 
-// Wipe if --full. Move, produce. Images if --full. Strip if --uncss.
-gulp.task("build", ["move", "images", "strip-css"]);
+// Wipe first. Move, produce. Images if --full. Strip if --uncss.
+gulp.task("build", ["build-move", "build-images", "build-strip"]);
+
+gulp.task("styleguide", ["build-compile", "styleguide-compile"]);
